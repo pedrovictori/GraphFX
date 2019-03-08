41ceedf2b1c4073499490c611d3964b7d5974a33
Pedro Victori <fosforilasa@gmail.com>
2019-03-08 18:07:12 +0000

    Coord class

---
+++ src/main/java/core/Coord.java
@@ -0,0 +1,30 @@
+package core;
+
+/**
+ * Simple class to store pair of Doubles, referred as x and y.
+ */
+public class Coord {
+    private Double x;
+    private Double y;
+
+    public Coord(Double x, Double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coord{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
